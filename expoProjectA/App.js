6ee3ce3cd8c652e327f8e99f8e62402472ee0f4d
Adarsh Anchan <anchanadarsh@gmail.com>
2019-08-12 21:54:54 +0530

    react cli and expo setup

---
+++ expoProjectA/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.introText}>This is my first React Native Project!!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  introText: {
+    fontSize:20,
+    fontWeight:'bold',
+    color: '#00e676',
+  }
+});
